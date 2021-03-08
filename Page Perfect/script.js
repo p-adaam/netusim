3ba70dbd1ead2892c44c8341bbd3583c8246a423
p-adaam <80282274+p-adaam@users.noreply.github.com>
2021-03-08 20:01:27 +0100

    Add files via upload

---
+++ Page Perfect/script.js
@@ -0,0 +1,25 @@
+function expandNav() {
+	document.querySelector('#mobileNavigation').classList.toggle('expandedNav');
+	document.querySelector('#mobileNavigation').classList.toggle('collapsedNav');
+}
+
+function toggleCompleteWeb() {
+	document.querySelector('#completeWebModal').classList.toggle('activeModal');
+    document.querySelector('#completeWebModal').classList.toggle('inactiveModal');
+}
+
+function toggleIdeasWeb() {
+	document.querySelector('#ideasWebModal').classList.toggle('activeModal');
+    document.querySelector('#ideasWebModal').classList.toggle('inactiveModal');
+}
+
+function toggleDesignWeb() {
+	document.querySelector('#designWebModal').classList.toggle('activeModal');
+    document.querySelector('#designWebModal').classList.toggle('inactiveModal');
+}
+
+function toggleScriptWeb() {
+	document.querySelector('#scriptWebModal').classList.toggle('activeModal');
+    document.querySelector('#scriptWebModal').classList.toggle('inactiveModal');
+}
+
